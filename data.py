edb5cf56b17399ecd0bb10bc563486681b5c5731
Florent Delorme <uqfdelor@eait.uq.edu.au>
2025-05-13 14:50:47 +1000

    Updated for sp500 tech stocks

--- data.py
+++ data.py
@@ -3,6 +3,15 @@ import yfinance as yf
 
 RAW_DIR = "data/raw"
 
+sp500_tech = [
+    "MSFT", "AAPL", "NVDA", "AVGO", "ORCL", "PLTR", "CRM", "CSCO", "IBM",
+    "NOW", "ACN", "INTU", "AMD", "TXN", "ADBE", "QCOM", "AMAT", "PANW",
+    "ANET", "ADI", "CRWD", "LRCX", "MU", "APH", "KLAC", "INTC", "CDNS",
+    "SNPS", "FTNT", "DELL", "WDAY", "MSI", "ADSK", "ROP", "NXPI", "FICO",
+    "TEL", "CTSH", "GLW", "IT", "MPWR", "MCHP", "ANSS", "KEYS", "GDDY",
+    "HPQ", "VRSN"
+]
+
 def fetch_data(tickers, start_date, end_date):
     # Download historical data
     os.makedirs(RAW_DIR, exist_ok=True)
@@ -16,4 +25,4 @@ def fetch_data(tickers, start_date, end_date):
 
 if __name__ == "__main__":
     # Example invocation
-    fetch_data(["KO", "PEP"], "2013-01-02", "2021-09-28")
+    fetch_data(sp500_tech, "2020-05-01", "2025-05-01")
